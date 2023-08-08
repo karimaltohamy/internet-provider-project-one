1cb9708388fd57f6d0b7458eaaacd803825d5231
karimaltohamy <karimbadr2003@gmail.com>
2023-08-08 14:23:05 +0200

    commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// menu
+const iconMenu = document.querySelector(".icon_menu");
+const overflow = document.querySelector(".overflow");
+const menuMobile = document.querySelector(".menu_mobile");
+const btnCloseMenu = document.querySelector(".btn_close_menu");
+const links = document.querySelectorAll(".menu_mobile .links .link");
+
+iconMenu.addEventListener("click", () => {
+  overflow.classList.toggle("active");
+  menuMobile.classList.toggle("active");
+});
+
+btnCloseMenu.addEventListener("click", () => {
+  overflow.classList.remove("active");
+  menuMobile.classList.remove("active");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    overflow.classList.remove("active");
+    menuMobile.classList.remove("active");
+  });
+});
